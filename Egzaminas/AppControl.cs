c84ff5d4954b4a187bfc4355e9655447b1665bbd
Denis <denis.zamaro@gmail.com>
2020-06-12 12:25:32 +0300

    Add files via upload

---
+++ Egzaminas/AppControl.cs
@@ -0,0 +1,31 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace Pagrindine
+
+{
+    public class App
+    {
+        public bool State = true;
+        readonly PacientProcessor clientProcessor = new PacientProcessor();
+        readonly List<Pacientas> pacientas = PacientoData.AddPacientas();
+        public int CustomerId { get; set; }
+        public int PacientoId { get; private set; }
+
+        public void FirstMessage()
+        {
+
+            Console.WriteLine("Pasirinkite paciento id: ");
+            int.TryParse(Console.ReadLine(), out int result);
+            var PacientoId = result;
+            Console.WriteLine(PacientoId);
+        }
+
+    }
+
+    internal class PacientProcessor
+    {
+    }
+}
+
---
+++ Egzaminas/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Pagrindine
+{
+    class Program
+    {
+        static void Main()
+        {
+            AppControl controller = new AppControl();
+            controller.FirstMessage();
+        }
+    }
+
+    internal class AppControl
+    {
+    }
+}
